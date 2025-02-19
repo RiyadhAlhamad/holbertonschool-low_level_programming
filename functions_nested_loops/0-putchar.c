ca3fb05d19708e512a4b5dddfe8e934a15bdb1d5
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-02-19 15:23:50 +0000

    add some file

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _putchar - print _putchar
+ * Return : returnt zero
+ */
+
+int _putchar(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
