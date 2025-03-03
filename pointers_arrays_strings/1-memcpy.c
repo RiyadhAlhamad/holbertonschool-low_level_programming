971ec4538c27eb7a646108ee63b14e32d82ec514
Your Name <you@example.com>
2025-03-03 19:43:24 +0000

    edit file 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,8 +12,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 
-    for (index = 0; index < n; index++)
-	    dest[index] = src[index];
-	
-    return (dest);
+	for (index = 0; index < n; index++)
+		dest[index] = src[index];
+
+	return (dest);
 }
