491528682157354b11f4fcf4f924146a8be4b78e
Your Name <you@example.com>
2025-03-06 00:16:01 +0000

    edit file 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <main.h>
+
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
+	char *endptr;
 
-	if (argc < 1)
-		return (0);
-
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		strtol(argv[i], &endptr, 10);
+
+		if (*endptr != '\0')
 		{
-			printf("%s\n", "Error");
+			printf("Error\n");
 			return (1);
 		}
 		sum += atoi(argv[i]);
