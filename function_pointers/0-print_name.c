410c07f0d13598c4e0bb6724fb20ce837ea49080
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-03-18 22:23:41 +0300

    Update 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,6 +3,7 @@
  * print_name - Prints a name.
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
+ *Return : return 0 or 1
  */
 void print_name(char *name, void (*f)(char *))
 {
