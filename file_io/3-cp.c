161957e9b6c42acbf7b9b40c91b73a442c1fc7f7
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-04-05 05:48:19 +0000

    edit file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,4 +1,11 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
 /**
  * main - program to copy
  * @ac: argument count
