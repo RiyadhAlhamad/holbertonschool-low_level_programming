51319d2d20ea5cc208fe1d0c6b4eee3301bdf77e
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-04-05 05:32:54 +0000

    add file 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * create_file - Creates a file with the given name and writes content to it
+ * @filename: The name of the file to create
+ * @text_content: The content to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0, written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		written = write(fd, text_content, len);
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
