b14b3f8e4d4f66b1f536508680bd81e3e2cfcd31
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-04-05 04:03:31 +0000

    edit file 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,55 +2,69 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes the node at index index of a doubly linked list
+ * delete_node_at_head - deletes the head node of the list
+ * @head: double pointer to the head of the doubly linked list
+ *
+ * Return: 1 if succeeded, -1 if failed
+ */
+int delete_node_at_head(dlistint_t **head)
+{
+    if (*head == NULL)
+    {
+        return (-1);
+    }
+
+    dlistint_t *temp = *head;
+
+    *head = temp->next;
+    if (*head != NULL)
+    {
+        (*head)->prev = NULL;
+    }
+
+    free(temp);
+    return (1);
+}
+
+/**
+ * delete_node_at_index - deletes a node at a specific index
  * @head: double pointer to the head of the doubly linked list
  * @index: index of the node to be deleted
  *
  * Return: 1 if succeeded, -1 if failed
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+int delete_node_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
-	unsigned int i;
+    dlistint_t *current;
+    unsigned int i;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
+    current = *head;
 
-	current = *head;
+    for (i = 0; current != NULL && i < index; i++)
+    {
+        current = current->next;
+    }
 
-	for (i = 0; current != NULL && i < index; i++)
-	{
-		current = current->next;
-	}
+    if (current == NULL)
+    {
+        return (-1);
+    }
 
-	if (current == NULL)
-	{
-		return (-1);
-	}
+    if (current == *head)
+    {
+        return (delete_node_at_head(head));
+    }
 
-	if (current == *head)
-	{
-		*head = current->next;
-		if (*head != NULL)
-		{
-			(*head)->prev = NULL;
-		}
-	}
-	else
-	{
-		if (current->prev != NULL)
-		{
-			current->prev->next = current->next;
-		}
+    if (current->prev != NULL)
+    {
+        current->prev->next = current->next;
+    }
 
-		if (current->next != NULL)
-		{
-			current->next->prev = current->prev;
-		}
-	}
+    if (current->next != NULL)
+    {
+        current->next->prev = current->prev;
+    }
 
-	free(current);
-	return (1);
+    free(current);
+    return (1);
 }
