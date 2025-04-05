5eeda4c75b9746561a770a2ed5b527693843bc21
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-04-05 04:24:59 +0000

    add file

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,8 @@
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: a string containing the binary number
  *
- * Return: the converted number, or 0 if there is an invalid character or b is NULL
+ * Return: the converted number, or 0 if there is
+ * an invalid character or b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
