5bc90b0cac71487e3831b254a9a9c54b4028fcb5
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-04-05 08:53:52 +0300

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,60 +1,69 @@
-#include"main.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<errno.h>
-#include<string.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
 /**
- *main-program to copy
- *@ac: argument count
- *@av: array of arguments
- *Return: a value
+ * main - program to copy
+ * @ac: argument count
+ * @av: array of arguments
+ * Return: a value
  */
-int main(int ac,char**av)
+int main(int ac, char **av)
 {
-	int fdFrum,fdToo,wrote,readed;
-	char buff[1024];
-	if(ac!=3)
-	{
-		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fdFrum=open(av[1],O_RDONLY);
-	if(fdFrum==-1)
-	{
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n",av[1]);
-		exit(98);
-	}
-	fdToo=open(av[2],O_CREAT|O_WRONLY|O_TRUNC,0664);
-	if(fdToo==-1)
-	{
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n",av[2]);
-		exit(99);
-	}
-	while((readed=read(fdFrum,buff,1024))>0)
-	{
-		wrote=write(fdToo,buff,readed);
-		if(wrote==-1)
-		{
-			dprintf(STDERR_FILENO,"Error: Can't write to %s\n",av[2]);
-			exit(99);
-		}
-	}
-	if(readed==-1)
-	{
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n",av[1]);
-		exit(98);
-	}
-	if(close(fdFrum)==-1)
-	{
-		dprintf(STDERR_FILENO,"Error: Can't close fd %d",fdFrum);
-		exit(100);
-	}
-	if(close(fdToo)==-1)
-	{
-		dprintf(STDERR_FILENO,"Error: Can't close fd %d",fdToo);
-		exit(100);
-	}
-	return(0);
+    int fdFrum, fdToo, wrote, readed;
+    char buff[1024];
+
+    if (ac != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+
+    fdFrum = open(av[1], O_RDONLY);
+    if (fdFrum == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+        exit(98);
+    }
+
+    fdToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (fdToo == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+        exit(99);  // يمكن أن يبقى هنا أو يتم تغييره إلى 98 حسب المتطلبات
+    }
+
+    while ((readed = read(fdFrum, buff, 1024)) > 0)
+    {
+        wrote = write(fdToo, buff, readed);
+        if (wrote == -1)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+            exit(99); // راجع هذه القيم أيضًا
+        }
+    }
+
+    if (readed == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+        exit(98);
+    }
+
+    if (close(fdFrum) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrum);
+        exit(100);
+    }
+
+    if (close(fdToo) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdToo);
+        exit(100);
+    }
+
+    return (0);
 }
